570b7f37760af3632b3b7ec9704bd8f8d9bdac13
Martim Gaspar <mgaspar@unc.edu>
2024-10-05 15:20:03 -0400

    Added Materials and roughness - Forgot to push.

--- RayTracer/src/Renderer.cpp
+++ RayTracer/src/Renderer.cpp
@@ -70,13 +70,13 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
 	glm::vec3 color(0.0f);
 	float multiplier = 1.0f;
 
-	int bounces = 2;
+	int bounces = 5;
 	for (int i = 0; i < bounces; i++)
 	{
 		Renderer::HitPayload payload = TraceRay(ray);
 		if (payload.HitDistance < 0.0f)
 		{
-			glm::vec3 skyColor = glm::vec3(0.0f, 0.0f, 0.0f);
+			glm::vec3 skyColor = glm::vec3(0.6f, 0.7f, 0.9f);
 			color += skyColor * multiplier;
 			break;
 		}
@@ -85,14 +85,16 @@ glm::vec4 Renderer::PerPixel(uint32_t x, uint32_t y)
 		float lightIntensity = glm::max(glm::dot(payload.WorldNormal, -lightDir), 0.0f); // == cos(angle)
 
 		const Sphere& sphere = m_ActiveScene->Spheres[payload.ObjectIndex];
-		glm::vec3 sphereColor = sphere.Albedo;
+		const Material& material = m_ActiveScene->Materials[sphere.MaterialIndex];
+		glm::vec3 sphereColor = material.Albedo;
 		sphereColor *= lightIntensity;
 		color += sphereColor * multiplier;
 
-		multiplier *= 0.7f;
+		multiplier *= 0.5f; //Reducing the sphere color so it doesnt get infinetely brighter with each bounce.
 
-		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f;
-		ray.Direction = glm::reflect(ray.Direction, payload.WorldNormal);
+		ray.Origin = payload.WorldPosition + payload.WorldNormal * 0.0001f; // Slightly moving the bounced rays so they dont bounce with the initial ray.
+		ray.Direction = glm::reflect(ray.Direction,
+			payload.WorldNormal + material.Roughness * Walnut::Random::Vec3(-0.5f, 0.5f));
 	}
 
 	return glm::vec4(color, 1.0f);
@@ -136,8 +138,8 @@ Renderer::HitPayload Renderer::TraceRay(const Ray& ray)
 		// (-b +- sqrt(discriminant)) / 2a
 
 		// float t0 = (-b + glm::sqrt(discriminant)) / (2.0f * a); // Second hit distance (currently unused)
-		float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a);
-		if (closestT > 0.0f && closestT < hitDistance)
+		float closestT = (-b - glm::sqrt(discriminant)) / (2.0f * a); //Calculates the closest hit/sphere
+		if (closestT > 0.0f && closestT < hitDistance) // Some voodoo magic to make sure the sphere isnt behind the camera
 		{
 			hitDistance = closestT;
 			closestSphere = (int)i;
